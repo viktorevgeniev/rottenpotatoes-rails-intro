5ec1ef39a7a2cea8ce39f068151beceadaadb894
Viktor Evgeniev <viktor.evgeniev@gmail.com>
2015-11-13 13:11:14 +0000

    hw 2 part 1 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
+    #css_element_id = column == sort_column
     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
     link_to title, :sort => column, :direction => direction
   end
